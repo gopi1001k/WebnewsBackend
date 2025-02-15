73067d25852817c7d25975b01ae5b1ec6e62739b
gopi1001k <koyalkargopikrishna@gmail.com>
2025-02-15 15:56:00 +0530

    Update content_Aggregate_RestAPI.py

--- content_Aggregate_RestAPI.py
+++ content_Aggregate_RestAPI.py
@@ -84,7 +84,6 @@ def catch_all(path=None):
     
 if __name__ == '__main__':
     # app.run(host="0.0.0.0",port=7000,debug=True)
-    port = int(os.environ.get("PORT", 7000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(debug=True)
 
 
